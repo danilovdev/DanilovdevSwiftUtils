772034f890904e5e325f4836e6219d051ba2a45b
Данилов Алексей Николаевич <ANDanilov@alfabank.ru>
2024-02-25 19:27:44 +0700

    Short and convenient activate() method

--- Sources/DanilovdevSwiftUtils/UIKit/NSLayoutConstraint/NSLayoutConstraint+Activate.swift
+++ Sources/DanilovdevSwiftUtils/UIKit/NSLayoutConstraint/NSLayoutConstraint+Activate.swift
@@ -1,8 +1,32 @@
 //
-//  File.swift
+//  NSLayoutConstraint+Activate.swift
 //  
 //
 //  Created by Aleksei Danilov on 24.02.2024.
 //
 
-import Foundation
+import UIKit
+
+public extension NSLayoutConstraint {
+    @discardableResult
+    func activate() -> NSLayoutConstraint {
+        isActive = true
+        return self
+    }
+    
+    @discardableResult
+    func deactivate() -> NSLayoutConstraint {
+        isActive = false
+        return self
+    }
+}
+
+public extension Array where Element == NSLayoutConstraint {
+    func activate() {
+        NSLayoutConstraint.activate(self)
+    }
+    
+    func deactivate() {
+        NSLayoutConstraint.deactivate(self)
+    }
+}
--- Sources/DanilovdevSwiftUtils/UIKit/UIView/UIView+Constraints.swift
+++ Sources/DanilovdevSwiftUtils/UIKit/UIView/UIView+Constraints.swift
@@ -1,6 +1,6 @@
 //
 //  UIView+Constraints.swift
-//  
+//
 //
 //  Created by Aleksei Danilov on 23.01.2023.
 //
@@ -16,10 +16,12 @@ public extension UIView {
         bottom: CGFloat = 0
     ) {
         translatesAutoresizingMaskIntoConstraints = false
-        leadingAnchor.constraint(equalTo: toView.leadingAnchor, constant: leading).isActive = true
-        topAnchor.constraint(equalTo: toView.topAnchor, constant: top).isActive = true
-        trailingAnchor.constraint(equalTo: toView.trailingAnchor, constant: -trailing).isActive = true
-        bottomAnchor.constraint(equalTo: toView.bottomAnchor, constant: -bottom).isActive = true
+        [
+            leadingAnchor.constraint(equalTo: toView.leadingAnchor, constant: leading),
+            topAnchor.constraint(equalTo: toView.topAnchor, constant: top),
+            trailingAnchor.constraint(equalTo: toView.trailingAnchor, constant: -trailing),
+            bottomAnchor.constraint(equalTo: toView.bottomAnchor, constant: -bottom)
+        ].activate()
     }
     
     func addConstraints(
@@ -27,27 +29,31 @@ public extension UIView {
         insets: UIEdgeInsets
     ) {
         translatesAutoresizingMaskIntoConstraints = false
-        leadingAnchor.constraint(equalTo: toView.leadingAnchor, constant: insets.left).isActive = true
-        topAnchor.constraint(equalTo: toView.topAnchor, constant: insets.top).isActive = true
-        trailingAnchor.constraint(equalTo: toView.trailingAnchor, constant: -insets.right).isActive = true
-        bottomAnchor.constraint(equalTo: toView.bottomAnchor, constant: -insets.bottom).isActive = true
+        [
+            leadingAnchor.constraint(equalTo: toView.leadingAnchor, constant: insets.left),
+            topAnchor.constraint(equalTo: toView.topAnchor, constant: insets.top),
+            trailingAnchor.constraint(equalTo: toView.trailingAnchor, constant: -insets.right),
+            bottomAnchor.constraint(equalTo: toView.bottomAnchor, constant: -insets.bottom)
+        ].activate()
     }
     
     func makeCenterEqualsToSuperview() {
         translatesAutoresizingMaskIntoConstraints = false
         guard let superview = superview else { return }
-        centerYAnchor.constraint(equalTo: superview.centerYAnchor).isActive = true
-        centerXAnchor.constraint(equalTo: superview.centerXAnchor).isActive = true
+        [
+            centerYAnchor.constraint(equalTo: superview.centerYAnchor),
+            centerXAnchor.constraint(equalTo: superview.centerXAnchor)
+        ].activate()
     }
     
     func setHeight(_ value: CGFloat) {
         translatesAutoresizingMaskIntoConstraints = false
-        heightAnchor.constraint(equalToConstant: value).isActive = true
+        heightAnchor.constraint(equalToConstant: value).activate()
     }
     
     func setWidth(_ value: CGFloat) {
         translatesAutoresizingMaskIntoConstraints = false
-        widthAnchor.constraint(equalToConstant: value).isActive = true
+        widthAnchor.constraint(equalToConstant: value).activate()
     }
 }
 
