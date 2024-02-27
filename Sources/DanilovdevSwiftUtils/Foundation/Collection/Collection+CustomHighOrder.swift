8d8e473f949a093e21000eb140d58a615290a0b7
Данилов Алексей Николаевич <ANDanilov@alfabank.ru>
2024-02-28 00:33:08 +0700

    Add custom map function to Collection protocol

---
+++ Sources/DanilovdevSwiftUtils/Foundation/Collection/Collection+CustomHighOrder.swift
@@ -0,0 +1,16 @@
+//
+//  Collection+CustomHighOrder.swift
+//
+//
+//  Created by Aleksei Danilov on 26.02.2024.
+//
+
+import Foundation
+
+public extension Collection {
+    
+    func customMap<T>(_ modifier: (Element) -> T) -> [T] {
+        var result: [T] = []
+        return result
+    }
+}
